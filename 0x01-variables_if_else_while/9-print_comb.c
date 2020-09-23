5a68dbbd96998aebc20c293ec4f9390691d8fe69
Khalah Jones-Golden <khasan222@gmail.com>
2020-09-23 15:26:09 +0000

    experimenting with different values

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		if (i != 57)
+		if (i != '9')
 		{
 			putchar(',');
 		}
-		if (i <= 56)
+		if (i <= '9')
 		{
 			putchar(' ');
 		}
